8209bab2c790ac2e36ad4408782708eaea786cea
Rami Leghrib <95674555+RamiLGH8@users.noreply.github.com>
2023-11-21 10:24:09 +0100

    i'v deleted home page and added splash screen

--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 Color backgroundColor = Colors.grey.shade900;
 Color blue = Colors.blue;
 Color secondBlue = Color.fromRGBO(92, 166, 176, 1);
+Color red = Colors.red;
